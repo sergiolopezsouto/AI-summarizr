3ec00c37f1564f061b663a0794b60fd09aa61dd9
sergiolopez <sergio.lopez.souto@gmail.com>
2023-08-03 00:21:19 +0200

    ready for deploy

--- README.md
+++ README.md
@@ -1,8 +1,70 @@
-# React + Vite
+# Article Summarizer
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This project is a React application that allows users to input an article URL and receive a short summary of it.
 
-Currently, two official plugins are available:
+## Table of Contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+1. [Features](#features)
+2. [Getting Started](#getting-started)
+3. [Usage](#usage)
+4. [Dependencies](#dependencies)
+5. [Contributing](#contributing)
+6. [License](#license)
+
+## Features
+
+- Ability to input an article URL and receive a short summary.
+- Stores previously searched articles in local storage.
+- Clipboard copy functionality for previously searched articles.
+- User-friendly error and loading states.
+
+## Getting Started
+
+Before you can run this project locally, you must have Node.js and npm installed on your machine.
+
+1. Clone this repository:
+   ```
+   git clone https://github.com/sergiolopezsouto/summarizerai.git
+   ```
+2. Navigate to the project directory:
+   ```
+   cd summarizerai
+   ```
+3. Install dependencies:
+   ```
+   npm install
+   ```
+4. Run the application:
+   ```
+   npm run dev
+   ```
+
+The application should now be running at [http://localhost:3000](http://localhost:3000).
+
+## Usage
+
+1. Input an article URL into the input field and press enter or click the submit button.
+2. The article summary will appear in the results section.
+3. Click on a previous article URL to copy it to your clipboard.
+
+## Dependencies
+
+This project is built with the following dependencies:
+
+- [React](https://reactjs.org/)
+- [Redux Toolkit](https://redux-toolkit.js.org/)
+- [React Redux](https://react-redux.js.org/)
+- [TailwindCSS](https://tailwindcss.com/)
+- And other minor dependencies which you can find in the package.json file.
+
+## Contributing
+
+Contributions are always welcome! Please read our contributing guidelines before making a pull request.
+
+## License
+
+This project is licensed under the terms of the MIT license. For more information, see the [LICENSE](./LICENSE) file.
+
+---
+
+Project built with 💙 by Sergio Lopez.
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,8 +8,18 @@ const Demo = () => {
     url: '',
     summary: '',
   })
+  const [allArticles, setAllArticles] = useState([])
+  const [copied, setCopied] = useState("")
 
   const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery()
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage)
+    }
+  }, [])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -18,28 +28,76 @@ const Demo = () => {
 
     if(data?.summary) {
       const newArticle = {...article, summary: data.summary}
+      const updatedAllArticles = [newArticle, ...allArticles]
       setArticle(newArticle)
-      console.log(newArticle)
+      setAllArticles(updatedAllArticles)
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
     }
   }
 
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(false), 3000)
+  }
+
+
   return (
     <section className="mt-16 w-full max-w-xl">
-      <div className="flex flex-col w-full gap-2">
 
-        {/* Search input */}
-        <form onSubmit={handleSubmit} className="relative flex justify-center items-center">
-          <img src={linkIcon} alt="link-icon" className="absolute left-0 my-2 ml-3 w-5"/>
-          <input type="url" placeholder='Enter a URL' value={article.url} onChange={(e) => setArticle({...article, url: e.target.value })} required className='url_input peer'/>
-          <button type='submit' className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'> ↵ </button>
-        </form>
+        <div className="flex flex-col w-full gap-2">
 
-        {/* URL History input */}
+            {/* Search input */}
+            <form onSubmit={handleSubmit} className="relative flex justify-center items-center">
+              <img src={linkIcon} alt="link-icon" className="absolute left-0 my-2 ml-3 w-5"/>
+              <input type="url" placeholder='Enter a URL' value={article.url} onChange={(e) => setArticle({...article, url: e.target.value })} required className='url_input peer'/>
+              <button type='submit' className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'> ↵ </button>
+            </form>
 
+            {/* URL History input */}
+            <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
+                {allArticles.map((item,index) => (
+                    <div key={`link-${index}`} onClick={() => setArticle(item)} className='link_card'>
+                        <div onClick={() => handleCopy(item.url)} className='copy_btn'>
+                          <img src={copied === item.url ? tick : copy} alt="copy-icon" className='w-[40%] h-[40%] object-contain' />
+                        </div>
+                        
+                        <p className='flex-1 fond-satoshi text-blue-700 font-medium text-sm truncate'>{item.url}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
 
-      </div>
-
-      {/* Results */}
+        {/* Results */}
+        <div className='my-10 max-w-full flex justify-center'>
+            {
+            isFetching ? 
+                (<img src={loader} alt='loader' className='w-20 h-20 object-contain'/>) 
+            : error ? 
+                  (
+                  <p className='font-inter font-bold text-black text-center'>
+                      Well, that was not supposed to happen...
+                      <br />
+                      <span className='font-satoshi font-normal text-gray-700'>
+                        {error?.data?.error}
+                      </span>
+                  </p>
+                  ) 
+                  : 
+                  (article.summary && 
+                      (
+                      <div className='flex flex-col gap-3'>
+                          <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+                            Article <span className='blue-gradient'>Summary</span>
+                          </h2>
+                          <div className='summary_box'>
+                            <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+                          </div>
+                      </div>
+                      )
+                  )
+            }
+        </div>
 
     </section>
   )
